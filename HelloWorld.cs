9021d805a2e4a420fbcbd8da38ed3a2149fb795d
Pablo <pablo.hehe2@gmail.com>
2025-07-25 23:36:09 -0300

    Create HelloWorld.cs
    1st Day

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //print("Hello Right from the start!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hello World from Update!");
+    }
+}
